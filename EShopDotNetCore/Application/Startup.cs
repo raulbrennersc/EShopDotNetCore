3310cc93f2abb421eba76fddb3474bd77a90ae57
Raul Costa <raulbrennersc@gmail.com>
2020-08-08 14:18:59 -0300

    Adicionando EFCore com SQLite e criando o DbContext

--- EShopDotNetCore/Application/Startup.cs
+++ EShopDotNetCore/Application/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Infra.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,7 @@ namespace Application
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<EShopContext>(x => x.UseLazyLoadingProxies().UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
         }
 
---
+++ EShopDotNetCore/Infra.Data/Context/EShopContext.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infra.Data.Context
+{
+    public class EShopContext : DbContext
+    {
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<CartItem> CartItems{ get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CategoryProduct> CategoryProducts { get; set; }
+        public DbSet<Coupon> Coupons { get; set; }
+        public DbSet<CreditCard> CreditCards { get; set; }
+        public DbSet<Favorite> Favorites { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderCoupon> OrderCoupons { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductDetail> ProductDetails { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public EShopContext(DbContextOptions<EShopContext> options) : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.HasDefaultSchema("eshop");
+        }
+    }
+}
