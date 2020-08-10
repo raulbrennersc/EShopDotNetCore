14a771901ace0425929faf7b6765d0f8ffdee999
Raul Costa <raulbrennersc@gmail.com>
2020-08-10 18:46:46 -0300

    Adicionando o ProductController

---
+++ Application/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+using System.Net;
+using Application.Helpers;
+using Domain.Dtos;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpPost]
+        public ActionResult GetFilteredProducts(ProductFilterDto filter)
+        {
+            var products = _productService.GetFilteredProducts(filter);
+            return HttpResponseHelper.Create(HttpStatusCode.OK, AppConstants.MSG_GENERIC_GET_SUCCESS, products);
+        }
+
+        [HttpGet]
+        [Route("{idProduct}")]
+        public ActionResult GetDetailedProduct(int productId)
+        {
+            var product = _productService.GetProductById(productId);
+            return HttpResponseHelper.Create(HttpStatusCode.OK, AppConstants.MSG_GENERIC_GET_SUCCESS, product);
+        }
+    }
+}
