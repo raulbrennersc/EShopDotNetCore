f25878a84cfc6fc2cc6a9cf6951a730104515d87
Raul Costa <raulbrennersc@gmail.com>
2020-08-10 22:08:36 -0300

    Adicionando seed de produtos e usu'arios

---
+++ Infra.Data/Seeds/Seed.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using Domain.Entities;
+using Infra.Data.Context;
+
+namespace Infra.Data.Seeds
+{
+    public class Seed
+    {
+        public static void SeedProducts(EShopContext context)
+        {
+            if (!context.Products.Any())
+            {
+                var productData = System.IO.File.ReadAllText("../Infra.Data/ProductSeed.json");
+                var products = JsonSerializer.Deserialize<List<Product>>(productData);
+                foreach (var product in products)
+                {
+                    context.Products.Add(product);
+                }
+                context.SaveChanges();
+            }
+        }
+
+        public static void SeedCustomers(EShopContext context)
+        {
+            if (!context.Customers.Any())
+            {
+                byte[] passwordHash, passwordSalt;
+                CreatePasswordHash("password", out passwordHash, out passwordSalt);
+                var customer = new Customer
+                {
+                    Cpf = "00000000000",
+                    BirthDate = new DateTime(2000, 01, 01),
+                    Email = "customer@email.com",
+                    FirstName = "Doom",
+                    LastName = "Guy",
+                    PasswordHash = passwordHash,
+                    PasswordSalt = passwordSalt,
+                };
+                context.Customers.Add(customer);
+                context.SaveChanges();
+            }
+        }
+
+        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
